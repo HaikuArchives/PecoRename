8a31ca44d8184650119f867b23d9bd7aa464bd2c
axeld <axeld@79d53f0e-920d-4bd1-9708-f0ba9553bdfd>
2011-03-05 20:41:32 +0000

    * Undefine __INTEL__ for now to make it build without regex. This needs to be   fixed later on for real. * Automatic whitespace cleanup.
    git-svn-id: http://svn.osdrawer.net/pecorename@5 79d53f0e-920d-4bd1-9708-f0ba9553bdfd

--- Main/Renamer_SearchReplace.cpp
+++ Main/Renamer_SearchReplace.cpp
@@ -21,6 +21,9 @@
 #include "functions.h"
 #include "LiveTextControl.h"
 
+// TODO: this effectively turns off this replace mode, and should be fixed instead
+#undef __INTEL__
+
 // Für dieses Token wird das Suchergebnis als String ersetzt!
 #define TOKEN "\\$"
 // Dies ist das geschützte Token -> wird durch das Token ersetzt!
@@ -47,17 +50,17 @@ Renamer_SearchReplace::Renamer_SearchReplace()
 
 	frame.left = 4.0;
 	frame.top += be_plain_font->Size() + 14;
-	
+
 	float			width;
-	
+
 	width = be_plain_font->StringWidth(REN_SET_SEARCHPATTERN);
-	SearchFor = new LiveTextControl( BRect(frame.left, frame.top, frame.left + width + 150, 0), NULL, REN_SET_SEARCHPATTERN, NULL, new BMessage(MSG_RENAME_SETTINGS));									
+	SearchFor = new LiveTextControl( BRect(frame.left, frame.top, frame.left + width + 150, 0), NULL, REN_SET_SEARCHPATTERN, NULL, new BMessage(MSG_RENAME_SETTINGS));
 	SearchFor->SetDivider(width + 5);
 	AddChild(SearchFor);
 
 	frame.left += width + 160;
 	width = be_plain_font->StringWidth(REN_SET_REPLACESTRING);
-	ReplaceWith = new LiveTextControl( BRect(frame.left, frame.top, frame.left + width + 150, 0), NULL, REN_SET_REPLACESTRING, NULL, new BMessage(MSG_RENAME_SETTINGS));									
+	ReplaceWith = new LiveTextControl( BRect(frame.left, frame.top, frame.left + width + 150, 0), NULL, REN_SET_REPLACESTRING, NULL, new BMessage(MSG_RENAME_SETTINGS));
 	ReplaceWith->SetDivider(width + 5);
 	AddChild(ReplaceWith);
 
@@ -66,7 +69,7 @@ Renamer_SearchReplace::Renamer_SearchReplace()
 void Renamer_SearchReplace::RenameList(BList *FileList) {
 
 	Renamer :: RenameList(FileList);
-	
+
 	// Daten aus dem Formular einlesen
 	const bool 	matchcase = bool(MatchCase->Value());		// Gross/Lleinschreibung beachten?
 #if __INTEL__
@@ -74,9 +77,9 @@ void Renamer_SearchReplace::RenameList(BList *FileList) {
 #endif
 	const char* utf_pattern = SearchFor->Text();			// Such-Pattern
 	const char* utf_replacestring = ReplaceWith->Text();	// Ersetzstring
-	
+
 	FileListItem	*ListItem;
-	
+
 	if 	(strlen(utf_pattern)==0) return;
 
 #if __INTEL__
@@ -84,76 +87,76 @@ void Renamer_SearchReplace::RenameList(BList *FileList) {
 
 		// Pattern und Replace-String von UTF nach ASCII ISO1 umwandeln
 		int32	laenge1, laenge2;
-		
+
 		laenge1 = laenge2 = strlen(utf_pattern);
 		char	*pattern = new char[laenge2 + 1];
 		convert_from_utf8(B_ISO1_CONVERSION, utf_pattern, &laenge1, pattern, &laenge2, 0);
 		pattern[laenge2] = 0;
-	
+
 		laenge1 = laenge2 = strlen(utf_replacestring);
 		char	*replacestring = new char[laenge2 + 1];
 		convert_from_utf8(B_ISO1_CONVERSION, utf_replacestring, &laenge1, replacestring, &laenge2, 0);
 		replacestring[laenge2] = 0;
-	
+
 		// Regulären Ausdruck anwenden
 		unsigned char	buffer[REG_BUFFER_SIZE];
-	
+
 		regex_t			my_regex = { (unsigned char *)&buffer, REG_BUFFER_SIZE, 0, RE_SYNTAX_POSIX_AWK, NULL, NULL, 0, 1, REGS_FIXED, 0, 1, 0, 0, 0 };
 
 		// Fehler im Regulären Ausdruck
 		if 	(regcomp ( &my_regex, pattern, matchcase ? 0 : REG_ICASE ) != 0) return;
-		
+
 		regmatch_t		my_regmatch = { 0, 0 };
-		
+
 		for (int32 i = 0; i < fNumberOfItems; i++ ) {
 			ListItem = (FileListItem *)FileList->ItemAt(i);
-			
+
 			BString		ResultString;
-			
+
 			laenge1 = laenge2 = strlen(ListItem->fName.String());
 			char	*str = new char[laenge2 + 1];
-			
+
 			convert_from_utf8(B_ISO1_CONVERSION, ListItem->fName.String(), &laenge1, str, &laenge2, 0);
 			str[laenge2] = 0;
-			
+
 			while ( regexec (&my_regex, str, 1, &my_regmatch, 0) == 0 ) {
-				
+
 				if  (my_regmatch.rm_eo == 0) { ResultString = ""; break; };
 				int laenge = my_regmatch.rm_eo - my_regmatch.rm_so;
 				char	*tokenstr = new char[laenge + 1];
 				strncpy(tokenstr, str + my_regmatch.rm_so, laenge );
 				tokenstr[laenge] = 0;
-				
+
 				BString		PatternString = replacestring;
 				PatternString.ReplaceAll(NO_TOKEN, "\1");
 				PatternString.ReplaceAll(TOKEN, tokenstr);
 				PatternString.ReplaceAll("\1", TOKEN);
-				
+
 				str[ my_regmatch.rm_so ] = 0;
-				
+
 				ResultString << str << PatternString.String();
-				
+
 				str += my_regmatch.rm_eo;
 			}
-			
+
 			ResultString << str;
-		
+
 			// Und alles wieder in UTF8 umwandeln
-			
+
 			laenge1 = strlen(ResultString.String());
 			laenge2 = laenge1 * 2;
 			char	*utf_String = new char[laenge2 + 1];
-		
+
 			convert_to_utf8(B_ISO1_CONVERSION, ResultString.String(), &laenge1, utf_String, &laenge2, 0);
 			utf_String[laenge2] = 0;
-		
+
 			ListItem->SetNewName( utf_String );
 		}
 
 	} else {
 #endif
 		for (int32 i = 0; i < fNumberOfItems; i++ ) {
-		
+
 			ListItem = (FileListItem *)FileList->ItemAt(i);
 
 			BString		ResultString( ListItem->fName );
@@ -185,7 +188,7 @@ void Renamer_SearchReplace::DetachedFromWindow() {
 void Renamer_SearchReplace::AttachedToWindow() {
 	BMessage	msg;
 	ReadPreferences("ren_searchreplace", msg);
-	
+
 	BString string;
 	bool boolean;
 	if (msg.FindBool("matchcase", &boolean)==B_OK)
