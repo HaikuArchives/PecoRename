73415280e6718d7091c78e0eee75d7ce9b70c6e8
Humdinger <humdingerb@gmail.com>
2018-01-15 08:40:45 +0100

    Localize Tracker add-on
    ...and improved the alert text.
    Use the standard Makefile.

--- Tracker Add-On/main.cpp
+++ Tracker Add-On/main.cpp
@@ -8,11 +8,15 @@
  *              Werner Freytag <freytag@gmx.de>
  */
 
-#include <Roster.h>
-#include <Entry.h>
-#include <TrackerAddOn.h>
 #include <Alert.h>
+#include <Catalog.h>
+#include <Entry.h>
 #include <Messenger.h>
+#include <Roster.h>
+#include <TrackerAddOn.h>
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "Tracker_addon"
 
 #define APP_SIGNATURE "application/x-vnd.pecora-PecoRename"
 
@@ -29,6 +33,11 @@ void process_refs(entry_ref dir_ref, BMessage *msg, void *) {
 	new BRoster();
 	if (be_roster->Launch(APP_SIGNATURE, (BMessage *)NULL, &team) == B_OK)
 		(new BMessenger(APP_SIGNATURE, team))->SendMessage(&theMessage);
-	else
-		(new BAlert("", "I'm sorry, I can't find PecoRename.\n\nIs it already installed on this system?\n\nYou can download it from\nhttp://www.pecora.de/pecorename !", "Ok, I will do that", NULL, NULL, B_WIDTH_AS_USUAL, B_STOP_ALERT))->Go();
+	else {
+		(new BAlert("", B_TRANSLATE("Can't find PecoRename.\n\n"
+			"It looks like it's not properly installed.\n"
+			"The PecoRename package can be downloaded and installed "
+			"with HaikuDepot."), B_TRANSLATE("OK"),
+			NULL, NULL, B_WIDTH_AS_USUAL, B_STOP_ALERT))->Go();
+	}
 };
